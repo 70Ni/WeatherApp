d9a3b72f8f2d18822bff31d06cb5125cbefeb7eb
70ni <4thedevelopments@gmail.com>
2024-05-20 20:01:35 +0530

    Search input section

--- src/Components/SerachInput/SerachInput.jsx
+++ src/Components/SerachInput/SerachInput.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./searchInput.css";
 import Search from "../../Images/Icon/search.svg";
+import google from "../../Images/Icon/google.svg";
+import arrow from "../../Images/Icon/Arrow.svg";
 
 
 function SerachInput() {
@@ -8,7 +10,9 @@ function SerachInput() {
     <div className="searchInputwrpr" id="seachInput">
       <form action="" className="searchform">
         <img src={Search} alt="search" className="searchIcon" />
-        <input type="text" />
+        <input type="text" placeholder="search"/>
+        <img src={google} alt="Google icon" className="GoogleIcon" />
+        <img src={arrow} alt="search" className="EnterIcon" />
       </form>
     </div>
   );
